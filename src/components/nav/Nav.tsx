4a9796e44fd4ae1107580e422e5f2b3596a6cee8
Sean Kelly <seangerrykelly@gmail.com>
2025-02-18 23:57:38 -0500

    fix: close mobile nav links if you click anywhere outside the menu

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,13 +1,30 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { NavBar, NavHomeLink, NavLinks, NavLink, NavMenuMobileButton } from "./NavStyles"
+
 export const Nav = () => {
     const [isOpen, setIsOpen] = useState<boolean>(false)
+    const navLinksRef = useRef<HTMLUListElement>(null)
+
+    const handleClickOutside = (event: MouseEvent) => {
+        if (navLinksRef.current && !navLinksRef.current.contains(event.target as Node)) {
+            setIsOpen(false)
+        }
+    }
+
+    useEffect(() => {
+        if (isOpen) {
+            document.addEventListener("mousedown", handleClickOutside)
+        } else {
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+        return () => document.removeEventListener("mousedown", handleClickOutside)
+    })
 
     return (
         <NavBar>
             <NavHomeLink href="/">Algorithm Visualizer</NavHomeLink>
             <NavMenuMobileButton onClick={() => setIsOpen(!isOpen)}>☰</NavMenuMobileButton>
-            <NavLinks className={isOpen ? "open" : ""}>
+            <NavLinks ref={navLinksRef} className={isOpen ? "open" : ""}>
                 <NavLink href="/sorting">Sorting</NavLink>
                 <NavLink href="/searching">Searching</NavLink>
                 <NavLink href="/pathfinding">Pathfinding</NavLink>
